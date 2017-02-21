44ffda5fde79f25908d2d2e8864ddd661329ee63
DnyaneshwarFeast <dnyaneshwar.galande@feastsoftware.com>
2017-02-21 11:30:35 +0530

    Update Program.cs

--- sol_HelloWorld/sol_HelloWorld/Program.cs
+++ sol_HelloWorld/sol_HelloWorld/Program.cs
@@ -11,6 +11,7 @@ namespace sol_HelloWorld
         static void Main(string[] args)
         {
             System.Console.WriteLine("Hello World");
+              System.Console.WriteLine("Hello from github");
         }
     }
 }
